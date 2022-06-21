780cb3270944d43fb04fb014f4c81b50d0d2aa5e
Irina Trubchik <trubchik@mail.ru>
2022-06-21 19:11:32 +0300

    rename rout-event-list-view to list-view , доб наслед-е прост комп

---
+++ src/view/list-view.js
@@ -0,0 +1,10 @@
+import AbstractView from '../framework/view/abstract-view.js';
+
+const createNewListTemplate = ()=>'<ul class="trip-events__list"></ul>';
+
+export default class ListView extends AbstractView {
+
+  get template() {
+    return createNewListTemplate();
+  }
+}
